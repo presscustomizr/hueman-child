f3fcc026ae14fe5a2ff1084524507d944b43cdbf
Alexander Agnarson <alexander.agnarson@gmail.com>
2013-12-04 09:29:44 +0100

    Initial commit

---
+++ functions.php
@@ -0,0 +1,7 @@
+<?php
+/* ------------------------------------------------------------------------- *
+ *  Custom functions
+/* ------------------------------------------------------------------------- */
+	
+	// Add your custom functions here, or overwrite existing ones. Read more how to use:
+	// http://codex.wordpress.org/Child_Themes
